c46eaf2dcaece51962a69ba89730219997871dfe
george <george.duffett@icloud.com>
2022-10-09 18:22:23 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CONFIG_MESSAGE='Link to personal config'
+CONFIG_FILES_DIRECTORY=$(pwd)
+
+setup_zsh() {
+    config_location=~/.zshrc
+    echo ${CONFIG_MESSAGE} >> ~
+    echo "source ${CONFIG_FILES_DIRECTORY}/aliases.sh" >> ${config_location}
+    echo "source ${CONFIG_FILES_DIRECTORY}/.zshrc" >> ${config_location}
+}
+
+setup_bash() {
+    config_location=~/.bashrc
+    echo ". ${CONFIG_FILES_DIRECTORY}/aliases.sh" >> ${config_location}
+    echo ". ${CONFIG_FILES_DIRECTORY}/.bashrc" >> ${config_location}
+}
+
+[[ -f ~/.zshrc ]] && setup_zsh
+
+[[ -f ~/.bashrc ]] && setup_bash
