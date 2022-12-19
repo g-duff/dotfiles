a50867b2c25db4065b7886132ee646345991b89d
george <george.duffett@icloud.com>
2022-12-19 00:50:24 +0000

    Add fugitive as a dependency

--- nvim/lua/g-duff/packer.lua
+++ nvim/lua/g-duff/packer.lua
@@ -6,5 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-end)
 
+  -- Git
+  use 'tpope/vim-fugitive'
+end)
