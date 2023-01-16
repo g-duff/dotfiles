70bc3f5e5c083187a93ace99d4eb484cb1142228
George Duffett <gduffett@financialforce.com>
2023-01-16 12:00:01 +0000

    Add project grep keymap

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,3 +6,6 @@ vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 -- Project all (files)
 vim.keymap.set('n', '<leader>pa', builtin.find_files, {})
 
+-- Project grep
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+
