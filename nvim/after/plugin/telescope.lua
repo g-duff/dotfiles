b02ace18792b7d7f133c7201ebe7612092d9c2d3
George Duffett <gduffett@financialforce.com>
2023-01-16 11:58:14 +0000

    Change find mneumonics

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 
--- Find files
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+-- Project files
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 
--- Find all
-vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
+-- Project all (files)
+vim.keymap.set('n', '<leader>pa', builtin.find_files, {})
 
