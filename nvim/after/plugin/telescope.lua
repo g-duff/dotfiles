dd95b574eb321ed2916419183d85302ac4be4dcf
George Duffett <gduffett@financialforce.com>
2023-01-26 22:52:14 +0000

    Add project buffers keymap

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,6 +6,9 @@ vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 -- Project all (files)
 vim.keymap.set('n', '<leader>pa', builtin.find_files, {})
 
+-- Project buffers
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+
 -- Project grep
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 
