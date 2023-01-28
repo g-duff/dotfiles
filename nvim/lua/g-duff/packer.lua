3f03b461cf3e84a2a942fc83949bdc54bb9f62cc
george <george.duffett@icloud.com>
2023-01-28 09:42:17 +0000

    Change colorscheme to catppuccin

--- nvim/lua/g-duff/packer.lua
+++ nvim/lua/g-duff/packer.lua
@@ -4,44 +4,38 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- Git
-  use 'tpope/vim-fugitive'
-  
-  -- Comments
-  use 'tpope/vim-commentary'
+	-- Git
+	use 'tpope/vim-fugitive'
+
+	-- Comments
+	use 'tpope/vim-commentary'
 
 
-  -- Colorscheme
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+	-- Colorscheme
+	use { 'catppuccin/nvim', as = 'catppuccin' }
 
-  -- Fuzzy finding
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	-- Fuzzy finding
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  -- Syntax tree
-  use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function()
-		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		  ts_update()
-	  end,
-  }
+	-- Syntax tree
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
 
-  -- Status line
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+	-- Status line
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
 end)
