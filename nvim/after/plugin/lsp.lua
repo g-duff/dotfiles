3d5f496db0e7c87f5ea6fe563120107a43490f08
George Duffett <gduffett@financialforce.com>
2023-02-11 18:22:07 +0000

    Add typescript lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -41,6 +41,11 @@ require('lspconfig')['pyright'].setup{
 	flags = lsp_flags,
 }
 
+require('lspconfig')['tsserver'].setup{
+	on_attach = on_attach,
+	flags = lsp_flags,
+}
+
 require('lspconfig')['rust_analyzer'].setup{
 	on_attach = on_attach,
 	flags = lsp_flags,
