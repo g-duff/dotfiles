b0a7aa3c76e38504083a2866007f3590519edd1c
george <george.duffett@icloud.com>
2023-06-11 01:16:59 +0100

    Add iso date command

--- nvim/lua/g-duff/commands.lua
+++ nvim/lua/g-duff/commands.lua
@@ -4,3 +4,15 @@ vim.api.nvim_create_user_command('ToReg', function(opts)
 	vim.cmd(vim_command)
 end, { nargs = "+" })
 
+vim.api.nvim_create_user_command('ToIsoDate', function(opts)
+
+	local range_index = opts.range + 1
+	local ranges = {
+		'1,$',
+		opts.line1,
+		opts.line1 .. ',' .. opts.line2
+	}
+
+	vim.cmd(ranges[range_index] .. " luado return string.gsub(line, \"(%d%d)/(%d%d)/(%d%d%d%d)\", \"%3-%2-%1\")")
+
+end, { range = true })
