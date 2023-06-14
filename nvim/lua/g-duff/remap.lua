8f615596656a130763ae40eb30192608ebd7414c
George Duffett <gduffett@certinia.com>
2023-06-14 12:50:56 +0100

    feat: add leader window navigation

--- nvim/lua/g-duff/remap.lua
+++ nvim/lua/g-duff/remap.lua
@@ -14,3 +14,9 @@ vim.keymap.set({'n', 'v'}, '<leader>D', '"+D') -- delete line
 vim.keymap.set('n', '<leader>p', '"+p')  -- paste after cursor
 vim.keymap.set('n', '<leader>P', '"+P')  -- paste before cursor
 
+-- Window navigation
+vim.keymap.set('n', '<leader>wh', '<C-w>h')
+vim.keymap.set('n', '<leader>wj', '<C-w>j')
+vim.keymap.set('n', '<leader>wk', '<C-w>k')
+vim.keymap.set('n', '<leader>wl', '<C-w>l')
+
