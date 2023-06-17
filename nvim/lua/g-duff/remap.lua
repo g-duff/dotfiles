e571351cfdfb14020b012977d550d0152257f536
george <george.duffett@icloud.com>
2023-06-17 23:16:04 +0100

    feat: add leader tab navigation

--- nvim/lua/g-duff/remap.lua
+++ nvim/lua/g-duff/remap.lua
@@ -20,3 +20,7 @@ vim.keymap.set('n', '<leader>wj', '<C-w>j')
 vim.keymap.set('n', '<leader>wk', '<C-w>k')
 vim.keymap.set('n', '<leader>wl', '<C-w>l')
 
+-- Tab navigation
+vim.keymap.set('n', '<leader>th', vim.cmd.tabprev)
+vim.keymap.set('n', '<leader>tl', vim.cmd.tabnext)
+
