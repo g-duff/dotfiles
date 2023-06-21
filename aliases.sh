8cbb36b983dc2de5bf71699b58eff63fd026f034
George Duffett <gduffett@certinia.com>
2023-06-21 16:19:03 +0100

    feat: add checkout, branch aliases

--- aliases.sh
+++ aliases.sh
@@ -14,12 +14,14 @@ alias nv='nvim'
 # Git
 alias g='git'
 alias gg='git status'
+alias gh='git checkout'
 alias gi='git diff'
 alias gis='git diff --staged'
 alias gk='git commit' # kommit
 alias gka='git commit --amend'
 alias gkan='git commit --amend --no-edit'
 alias gl='git log'
+alias gn='git branch'
 alias gp='git add' # put
 
 alias ghs='git push'
