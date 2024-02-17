d5dc7270bac0324f5bfdc3ebc5d27d454aa08468
George Duffett <gduffett@certinia.com>
2024-02-17 14:53:52 +0000

    feat: setup stow

--- setup.sh
+++ setup.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-CONFIG_MESSAGE='Link to personal config'
-CONFIG_FILES_DIRECTORY=$(pwd)
-
-setup_zsh() {
-    config_location=~/.zshrc
-    echo "# ${CONFIG_MESSAGE}" >> ${config_location}
-    echo "source ${CONFIG_FILES_DIRECTORY}/aliases.sh" >> ${config_location}
-    echo "source ${CONFIG_FILES_DIRECTORY}/.zshrc" >> ${config_location}
-}
-
-setup_neovim() {
-    ln -s $PWD/nvim ~/.config/
-}
-
-setup_zsh
-
-[[ -d ~/.config ]] && setup_neovim
+#!/bin/zsh
+stow --verbose --restow . --target ${HOME}
