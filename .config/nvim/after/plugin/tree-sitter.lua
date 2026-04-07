42e1417176cecd5c8572ed0ce677f72b570522e2
George Duffett <george.duffett@bbc.co.uk>
2026-04-07 23:10:23 +0100

    fix(nvim): treesitter

--- .config/nvim/after/plugin/tree-sitter.lua
+++ .config/nvim/after/plugin/tree-sitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "lua", "vim", "vimdoc", },
 
--- .config/nvim/lua/g-duff/plugins.lua
+++ .config/nvim/lua/g-duff/plugins.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = function()
 			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
